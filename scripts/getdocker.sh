45a1761cd4eceb9e900458bdb62ab40d255e2a35
serchaofan <624261309@qq.com>
2020-03-31 11:07:45 +0800

    200331

--- scripts/getdocker.sh
+++ scripts/getdocker.sh
@@ -1,4 +1,5 @@
-echo "Getting Docker..."
+# get docker
+echo -e "\033[31mGetting Docker...\033[0m"
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo https://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
@@ -6,6 +7,7 @@ yum install -y docker-ce
 systemctl start docker && systemctl enable docker
 echo "{\"registry-mirrors\": [\"https://docker.mirrors.ustc.edu.cn\"]}" > /etc/docker/daemon.json
 systemctl daemon-reload && systemctl restart docker
+sleep 1
 echo -e "\033[33m---------------Docker Installation Completed------------- \033[0m
 \033[33mDocker version:  \033[0m
 \033[33m$(docker version | head -n3 | egrep "Version|API version")\033[0m"
--- scripts/getk8s.sh
+++ scripts/getk8s.sh
@@ -1,4 +1,5 @@
-echo "Getting K8s..."
+# get k8s
+echo -e "\033[31mGetting K8s...\033[0m"
 cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
@@ -9,13 +10,13 @@ repo_gpgcheck=1
 gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
-yum makecache fast
+yum makecache fast -y
 yum install -y kubelet kubeadm kubectl
 systemctl enable kubelet && systemctl start kubelet
-echo "是否安装etcd? (y/n)"
+echo -e "\033[31m是否安装etcd? (y/n)\033[0m"
 read choose
 if [ $choose="y" -o $choose="yes" -o $choose="Y" ];then
-	  yum install -y etcd
+  yum install -y etcd
 fi
 
 echo "source <(kubectl completion bash)" >> ~/.bashrc
@@ -23,14 +24,20 @@ echo "source <(kubeadm completion bash)" >> ~/.bashrc
 source ~/.bashrc
 
 # initialize k8s
-echo "输入k8s服务子网"
+echo -e "\033[31m输入k8s服务子网\033[0m"
 read subnet
 cat << EOF > init-default.yml
+apiVersion: kubeadm.k8s.io/v1beta2
+kind: ClusterConfiguration
 imageRepository: registry.aliyuncs.com/google_containers
+kubernetesVersion: v$(rpm -qa | grep kubectl | awk -F'-' '{print $2}')
 networking:
-dnsDomain: cluster.local
-serviceSubnet: "$subnet"
+  serviceSubnet: "$subnet"
 EOF
 
-kubeadm config images list --config init-default.yml
+echo -e "\033[31m开始拉取镜像...\033[0m"
+# kubeadm config images list --config init-default.yml
 kubeadm config images pull --config init-default.yml
+
+echo -e "\033[33m---------------K8s Installation Completed-------------
+K8s version: v$(rpm -qa | grep kubectl | awk -F'-' '{print $2}') \033[0m"
