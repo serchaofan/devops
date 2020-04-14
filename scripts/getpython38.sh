f1eace35004527e1bec2c1325f84eb5cedebc73b
serchaofan <624261309@qq.com>
2020-04-14 10:11:24 +0800

    20200414

--- scripts/getpython38.sh
+++ scripts/getpython38.sh
@@ -1,4 +1,5 @@
 yum groups install -y Development\ Tools
 yum install -y libffi-devel openssl-devel
-curl -L https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz -o ~/Python-3.8.2.tgz && tar -xzf ~/Python-3.8.2.tgz -C ~/python38
-cd python38 && ./configure --prefix=/usr/local/python38 && make && make install &>/dev/null
+curl -L https://www.python.org/ftp/python/3.8.2/Python-3.8.2.tgz -o ~/Python-3.8.2.tgz && tar -xzf ~/Python-3.8.2.tgz
+cd ~/Python-3.8.2 && ./configure && make && make install
+echo -e "\033[31mPython版本：$(python3 --version)\033[0m"
